4aa8529abdb98a604536a60fbb502d731759755c
Mike Solomon <mike.solomon@hey.com>
2024-08-05 19:03:05 +0300

    Uses let binding

--- renderer/+onRenderClient.js
+++ renderer/+onRenderClient.js
@@ -3,12 +3,14 @@ export { onRenderClient };
 import { hydrate } from "../output/Vike.ClientRouting.Run";
 import { layout } from "../output/Vike.ClientRouting.Renderer.Layout";
 
+let dekuRoute;
+
 async function onRenderClient(pageContext) {
   if (pageContext.isHydration) {
     // discards unsubscribe
     const { route } = hydrate(pageContext.dekuHydrationData)(layout)(pageContext.Page)();
-    window.__dekuRoute = route;
+    dekuRoute = route;
   } else {
-    window.__dekuRoute(pageContext.Page)();
+    dekuRoute(pageContext.Page)();
   }
 }
